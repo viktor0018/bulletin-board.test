d33072abf04d367c7acf22feeb1b90ee1e814b3f
Виктор Каляев <viktor@iMac-Viktor.local>
2021-01-18 13:06:54 +0300

    add sanctum

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+class AuthController extends Controller
+{
+    public function login(Request $request){
+        /*$request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+            'device_name' => 'required',
+        ]);*/
+
+        $user = User::where('email', $request->email)->first();
+
+        if (! $user || ! Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect.'],
+            ]);
+        }
+
+        return $user->createToken('token-name', ['server:update'])->plainTextToken;
+    }
+
+    public function register(Request $request){
+        /*
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|confirmed|min:8',
+        ]);*/
+
+        Auth::login($user = User::create([
+            'name' => $request->name,
+            'surname' => $request->surname,
+            'middlename' => $request->middlename,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'user_role_id' => 1,
+            'phone' => $request->phone,
+            'user_status_id' => 1
+        ]));
+        event(new Registered($user));
+        return $user->createToken('token-name', ['server:update'])->plainTextToken;
+    }
+
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,67 +15,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
-
-use App\Http\Controllers\Controller;
-use App\Models\User;
-use App\Providers\RouteServiceProvider;
-use Illuminate\Auth\Events\Registered;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Hash;
-
-
-Route::post('/register', function (Request $request) {
-/*
-    $request->validate([
-        'name' => 'required|string|max:255',
-        'email' => 'required|string|email|max:255|unique:users',
-        'password' => 'required|string|confirmed|min:8',
-    ]);*/
-
-    Auth::login($user = User::create([
-        'name' => $request->name,
-        'surname' => $request->surname,
-        'middlename' => $request->middlename,
-        'email' => $request->email,
-        'password' => Hash::make($request->password),
-        'user_role_id' => 1,
-        'phone' => $request->phone,
-        'user_status_id' => 1
-    ]));
-
-    event(new Registered($user));
-
-
-
-     return $user->createToken('token-name', ['server:update'])->plainTextToken;
-});
-
+Route::post('/login',[AuthController::class, 'login']);
+Route::post('/register',[AuthController::class, 'register']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
-});
-
-
-use Illuminate\Validation\ValidationException;
-
-Route::post('/login', function (Request $request) {
-    /*$request->validate([
-        'email' => 'required|email',
-        'password' => 'required',
-        'device_name' => 'required',
-    ]);*/
-
-    $user = User::where('email', $request->email)->first();
-
-    if (! $user || ! Hash::check($request->password, $user->password)) {
-        throw ValidationException::withMessages([
-            'email' => ['The provided credentials are incorrect.'],
-        ]);
-    }
-
-    return $user->createToken('token-name', ['server:update'])->plainTextToken;
 });
